377c898d9e27dfc88968bdd7fbfe8e48a8714a2a
kepich <pav007prikhod@mail.ru>
2020-02-01 16:16:11 +0700

    [Add]: POST request completed! With editing

--- src/main/kotlin/com/restful/rest/controllers/TagControl.kt
+++ src/main/kotlin/com/restful/rest/controllers/TagControl.kt
@@ -1,17 +1,26 @@
 package com.restful.rest.controllers
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.bind.annotation.RequestMapping
+
 import org.springframework.beans.factory.annotation.Autowired
-import com.restful.rest.repos.TagRepository
-import com.restful.rest.model.Tag
-import com.restful.rest.repos.TaskRepository
-import com.restful.rest.model.Task
-import com.restful.rest.response.TagResponse
+
 import java.util.Optional
+
+import com.restful.rest.model.Tag
+import com.restful.rest.model.Task
+
+import com.restful.rest.repos.TagRepository
+import com.restful.rest.repos.TaskRepository
+
+import com.restful.rest.response.TagResponse
+
 import com.restful.rest.exceptions.NotFoundException
+import com.restful.rest.exceptions.DuplicateEntryException
 
 @RestController
 @RequestMapping("tag")
@@ -21,21 +30,41 @@ class TagController{
     @Autowired
     lateinit var task_repos: TaskRepository
 
-    // Debug only
-    @GetMapping()
-    fun tags() =
-        tag_repos.findAll()
+    
+    @PostMapping()
+    fun tag_create(@RequestBody tag: Tag): Tag{
+        var temp: Iterable<Tag> = tag_repos.findByTitle(tag.title)
+        if (temp.count() != 0){
+            var title_created: String = tag.title
+            throw DuplicateEntryException("Object Tag with title: $title_created already created")
+        }
+        return tag_repos.save(tag)
+    }
+
+    @PostMapping("{id}")
+    fun tag_update(@PathVariable id: Long, @RequestBody tag: Tag): Tag{
+        var temp: Optional<Tag> = tag_repos.findById(id)
+        if (!temp.isPresent()){
+            throw NotFoundException("Object Tag with tag: $id not found")
+        }
+        temp.get()
+        var new_tag: Tag = Tag(title=tag.title, id=id)
+
+        return tag_repos.save(new_tag)
+    }
+
+    // Debug only ***********************
+    @GetMapping("all")
+    fun all_tags() = tag_repos.findAll()
+    //***********************************
 
     @GetMapping("{id}")
     fun get_one_by_id(@PathVariable id: Long): TagResponse{
         var temp: Optional<Tag> = tag_repos.findById(id)
         if (!temp.isPresent())
-            throw NotFoundException("Object with tag: $id not found")
+            throw NotFoundException("Object Tag with tag: $id not found")
         
         var tag_with_tasks: TagResponse = TagResponse(temp.get(), task_repos.findByTaguid(id))
         return tag_with_tasks
     }
-    /*
-        Необходимо создать data class TagResponse, который будет содержать список задач и сам тег, чтобы потом вернуть его в виде JSON
-     */
 }
--- src/main/kotlin/com/restful/rest/controllers/TaskControl.kt
+++ src/main/kotlin/com/restful/rest/controllers/TaskControl.kt
@@ -1,10 +1,23 @@
 package com.restful.rest.controllers
+
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.PathVariable
+
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.bind.annotation.RequestMapping
+
 import org.springframework.beans.factory.annotation.Autowired
+
+import java.util.Optional
+
 import com.restful.rest.repos.TaskRepository
+import com.restful.rest.model.Task
+
+import com.restful.rest.exceptions.NotFoundException
+import com.restful.rest.exceptions.DuplicateEntryException
 
 @RestController
 @RequestMapping("task")
@@ -12,7 +25,26 @@ class TaskController{
     @Autowired
     lateinit var task_repos: TaskRepository
 
-    @GetMapping()
-    fun task() =
-        task_repos.findAll()
+    @PostMapping()
+    fun task_create_edit(@RequestBody task: Task): Task{
+        var temp: Iterable<Task> = task_repos.findByName(task.name)
+        if (temp.count() != 0){
+            var name_created: String = task.name
+            throw DuplicateEntryException("Object Task with name: $name_created already created")
+        }
+        return task_repos.save(task)
+    }
+
+    @PostMapping("{id}")
+    fun task_update(@PathVariable id: Long, @RequestBody task: Task): Task{
+        var temp: Optional<Task> = task_repos.findById(id)
+        if (!temp.isPresent()){
+            throw NotFoundException("Object Task with tag: $id not found")
+        }
+        temp.get()
+        var new_task: Task = Task(name=task.name, description=task.description, task_date=task.task_date, taguid=task.taguid, id=id)
+
+        return task_repos.save(new_task)
+    }
+
 }
