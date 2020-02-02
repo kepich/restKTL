d0389282ca61845ce66660083600c984aee2c83d
kepich <pav007prikhod@mail.ru>
2020-02-03 01:33:36 +0700

    Bugs fixed

--- src/main/kotlin/com/restful/rest/controllers/TaskControl.kt
+++ src/main/kotlin/com/restful/rest/controllers/TaskControl.kt
@@ -18,7 +18,10 @@ import org.springframework.beans.factory.annotation.Autowired
 import java.util.Optional
 
 import com.restful.rest.repos.TaskRepository
+import com.restful.rest.repos.TagRepository
+
 import com.restful.rest.model.Task
+import com.restful.rest.model.Tag
 
 import com.restful.rest.exceptions.NotFoundException
 import com.restful.rest.exceptions.DuplicateEntryException
@@ -29,6 +32,10 @@ class TaskController{
     @Autowired
     lateinit var task_repos: TaskRepository
 
+    @Autowired
+    lateinit var tag_repos: TagRepository
+
+
     @PostMapping()
     @ResponseStatus(HttpStatus.OK)
     fun task_create(@RequestBody task: Task){
@@ -37,6 +44,11 @@ class TaskController{
             var name_created: String = task.name
             throw DuplicateEntryException("Object Task with name: $name_created already created")
         }
+        var temp_tag: Optional<Tag> = tag_repos.findById(task.taguid)
+        if (!temp_tag.isPresent()){
+            var tag_id: Long = task.taguid
+            throw NotFoundException("Object Tag with id: $tag_id not found")
+        }
         task_repos.save(task)
     }
 
@@ -44,10 +56,13 @@ class TaskController{
     @ResponseStatus(HttpStatus.OK)
     fun task_update(@PathVariable id: Long, @RequestBody task: Task){
         var temp: Optional<Task> = task_repos.findById(id)
-        if (!temp.isPresent()){
-            throw NotFoundException("Object Task with tag: $id not found")
+        if (!temp.isPresent())
+            throw NotFoundException("Object Task with id: $id not found")
+        var temp_tag: Optional<Tag> = tag_repos.findById(task.taguid)
+        if (!temp_tag.isPresent()){
+            var tag_id: Long = task.taguid
+            throw NotFoundException("Object Tag with id: $tag_id not found")
         }
-        temp.get()
         task_repos.save(Task(name=task.name, description=task.description, task_date=task.task_date, taguid=task.taguid, id=id))
     }
 
